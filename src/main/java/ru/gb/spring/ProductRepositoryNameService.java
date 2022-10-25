07160b00eddc3333ddf6e108927fa621ee4164b4
W0iD <xcrypter@gmail.com>
2022-10-25 10:55:43 +0300

    HomeWork 2

--- src/main/java/ru/gb/spring/ProductRepositoryNameService.java
+++ src/main/java/ru/gb/spring/ProductRepositoryNameService.java
@@ -10,10 +10,9 @@ public class ProductRepositoryNameService {
     private static long productSequence = 1L;
 
     public void InitName(Product product) {
-        long sequence = productSequence++;
         Random random = new Random();
         DecimalFormat priceFormat = new DecimalFormat(".##");
-        product.setId(sequence);
+        product.setId(productSequence++);
         product.setTitle("Товар №" + random.nextInt(99));
         product.setPrice(priceFormat.format(999.99 * random.nextDouble()) + " руб.");
     }
